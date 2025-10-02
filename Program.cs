b1a2ce7ec816a02fa918ab6d7600dbc8ac0667c9
Ali Ata Can Gündüz <fairytail123567@gmail.com>
2025-10-02 18:03:57 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+string rocket = @"
+   /\
+  /  \
+  |  |
+  |[]|
+  |__|
+   /\
+  /  \
+   ||
+   ||";
+
+
+
+for (int i = 0; i < 10; i++)
+{
+    Console.Clear();
+    for(int j = 0; j < i; j++)
+    {
+        Console.WriteLine();
+    }
+    Console.WriteLine(rocket);
+    Thread.Sleep(2000);
+    if (i==9)
+    {
+        Console.WriteLine("**********************************");
+    }
+
+    
+    
+}
+Console.WriteLine("The rocket has landed.Woohoo!Another successful landing!");
